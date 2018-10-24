efd9f83a919045a4e3658b5bd88ef547ef4bc661
oumaereina <oumaereina@qq.com>
2018-10-24 10:02:23 +0800

    小修改 二级联动还是有问题

--- humenResource/src/main/java/com/oumae/controller/ResumeController.java
+++ humenResource/src/main/java/com/oumae/controller/ResumeController.java
@@ -6,6 +6,7 @@ import com.oumae.service.ResumeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -56,7 +57,35 @@ public class ResumeController {
     public String selectResume(HttpSession session, Model model) throws Exception{
         Visitor visitor = (Visitor) session.getAttribute("visitor");
         List<Resume> resumes = resumeService.selectResumeByVid(visitor.getV_id());
-        model.addAttribute("resumes",resumes);
+        session.setAttribute("resumes",resumes);
         return "visitorResume";
+    }
+    @RequestMapping("/updateResume")
+    public ModelAndView updateResume(HttpServletRequest request,HttpSession session, Model model) throws Exception{
+        Visitor visitor = (Visitor) session.getAttribute("visitor");
+        Resume resume = new Resume();
+        resume.setR_id(Integer.valueOf(request.getParameter("r_id")));
+        resume.setR_vid(Integer.valueOf(visitor.getV_id()));
+        resume.setR_name((String) request.getParameter("r_name"));
+        resume.setR_sex((String) request.getParameter("r_sex"));
+        resume.setR_nation((String) request.getParameter("r_nation"));
+        resume.setR_bir((String) request.getParameter("r_bir"));
+        resume.setR_native_place((String) request.getParameter("r_native_place"));
+        resume.setR_pol_sta((String) request.getParameter("r_pol_sta"));
+        resume.setR_height(Integer.valueOf(request.getParameter("r_height")));
+        resume.setR_weight(Integer.valueOf(request.getParameter("r_weight")));
+        resume.setR_health((String) request.getParameter("r_health"));
+        resume.setR_english((String) request.getParameter("r_english"));
+        resume.setR_edu((String) request.getParameter("r_edu"));
+        resume.setR_major(request.getParameter("r_major"));
+        resume.setR_idc(BigInteger.valueOf(Long.parseLong(request.getParameter("r_idc"))));
+        resume.setR_phone(BigInteger.valueOf(Long.parseLong(request.getParameter("r_phone"))));
+        resume.setR_add(request.getParameter("r_add"));
+        resume.setR_school((String) request.getParameter("r_school"));
+        resume.setR_hobby((String) request.getParameter("r_hobby"));
+        resume.setR_self((String) request.getParameter("r_self"));
+        resume.setR_state(1);
+        resumeService.updateResumeById(resume);
+        return new ModelAndView("redirect:selectResume");
     }
 }
--- humenResource/src/main/web/pages/visitorResume.jsp
+++ humenResource/src/main/web/pages/visitorResume.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: oumaereina
@@ -16,13 +17,56 @@
     <base href="<%=basePath%>"/>
     <title>游客简历管理</title>
 </head>
+<style>
+    .uu{
+        font-size: 14px;
+        list-style-type: none;
+    }
+    #u1{
+        font-size: 14px;
+        list-style-type: none;
+    }
+</style>
 <body>
 <div>
-    <ul>
+    <ul id="u1">
         <li><a href="resume/selectResume">查看简历</a></li>
         <li><a href="">修改简历</a></li>
     </ul>
 </div>
-${requestScope.resumes}
+<form action="resume/updateResume" method="post">
+    <div>
+       <c:forEach var="i" items="${sessionScope.resumes}">
+           <ul class="uu">
+               <li><input type="text" value="${i.getR_name()}" name="r_name"></li>
+               <li><input type="text"value="${i.getR_sex()}" name="r_sex"></li>
+               <li><input type="text"value="${i.getR_nation()}" name="r_nation"></li>
+               <li><input type="text"value="${i.getR_bir()}" name="r_bir"></li>
+               <li><input type="text"value="${i.getR_native_place()}" name="r_native_place"></li>
+               <li><input type="text"value="${i.getR_pol_sta()}" name="r_pol_sta"></li>
+               <li><input type="text"value="${i.getR_height()}" name="r_height"></li>
+               <li><input type="text"value="${i.getR_weight()}" name="r_weight"></li>
+               <li><input type="text"value="${i.getR_english()}" name="r_english"></li>
+               <li><input type="text"value="${i.getR_edu()}" name="r_edu"></li>
+               <li><input type="text"value="${i.getR_major()}" name="r_major"></li>
+               <li><input type="text"value="${i.getR_idc()}" name="r_idc"></li>
+               <li><input type="text"value="${i.getR_phone()}" name="r_phone"></li>
+               <li><input type="text"value="${i.getR_add()}" name="r_add"></li>
+               <li><input type="text"value="${i.getR_school()}" name="r_school"></li>
+               <li><input type="text"value="${i.getR_hobby()}" name="r_hobby"></li>
+               <li><input type="text"value="${i.getR_self()}" name="r_self">
+                   <input type="hidden" value="${i.getR_id()}" name="r_id">
+               </li>
+               <c:if test="${i.getR_state()==1}">
+                   <li>已读</li>
+               </c:if>
+               <c:if test="${i.getR_state()==0}">
+                   <li>未读</li>
+               </c:if>
+               <li><input type="submit" value="修改"></li>
+           </ul>
+       </c:forEach>
+    </div>
+</form>
 </body>
 </html>
